5df87d8b14257d98d37f48e06aca4e8839f37800
Paul MAZEAU <mazeau.paul@gmail.com>
2024-01-27 22:11:27 +0100

    Tab Navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+      <View style={styles.container}>
+        <Text>HomePage</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
